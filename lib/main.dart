213df09a9c2656334e491c1b65fb9735be2b4019
Anvar <cosmoasx@gmail.com>
2023-06-06 06:33:24 -0700

    background o'zgardi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tascounter/view/home_page.dart';
 
 void main(List<String> args) {
@@ -15,6 +16,8 @@ class myApp extends StatefulWidget {
 class _myAppState extends State<myApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.black));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -13,71 +13,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(40))),
-        title: const Center(
-          child: Text("Tasbeh"),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-                child: Column(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/back.jpg'), fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  "Zikrlar soni:",
-                  style: const TextStyle(fontSize: 40),
-                ),
+                Center(
+                    child: Column(
+                  children: [
+                    const Text(
+                      "Zikrlar soni:",
+                      style: TextStyle(fontSize: 50, color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      hisob.toString(),
+                      style: const TextStyle(fontSize: 55, color: Colors.white),
+                    ),
+                  ],
+                )),
                 const SizedBox(
-                  height: 50,
+                  height: 150,
                 ),
-                Text(
-                  hisob.toString(),
-                  style: const TextStyle(fontSize: 40),
-                ),
-              ],
-            )),
-            const SizedBox(
-              height: 150,
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  hisob++;
-                  if (hisob == 33) {
-                    zikr = "Alhamdulillah";
-                    hisob++;
-                  } else if (hisob == 66) {
-                    zikr = "La Ilha Illalloh";
-                    hisob++;
-                  } else if (hisob == 99) {
-                    zikr = "Subhanalloh";
-                    hisob = 0;
-                  }
-                });
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width * .8,
-                padding: const EdgeInsets.all(15),
-                child: Center(
-                  child: Text(
-                    zikr,
-                    style: TextStyle(fontSize: 30),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      hisob++;
+                      if (hisob == 33) {
+                        zikr = "Alhamdulillah";
+                        hisob++;
+                      } else if (hisob == 66) {
+                        zikr = "La Ilha Illalloh";
+                        hisob++;
+                      } else if (hisob == 99) {
+                        zikr = "Subhanalloh";
+                        hisob = 0;
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .8,
+                    padding: const EdgeInsets.all(15),
+                    child: Center(
+                      child: Text(
+                        zikr,
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
                   ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
